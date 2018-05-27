fb800b13882449c13d4e2082240044bd64dc6fa6
trioputrap <trioputrap@gmail.com>
2018-05-27 17:51:32 +0800

    new config

--- config.py
+++ config.py
@@ -1,41 +1,16 @@
 DELAY = 5
-ID_TB_INDEX = 0
 
 #database config
 DB_1 = {
-    'host': '10.10.23.203',
+    'host': '192.168.157.1',
     'user': 'trio',
     'pass': 'trio',
-    'db_name': 'ims_1'
+    'db_name': 'praktikum_ims_1'
 }
 
 DB_2 = {
-    'host': '10.10.23.39',
-    'user': 'pandika',
-    'pass': 'pandika',
-    'db_name': 'ims_1'
-}
-
-DB_3 = {
-    #IP menyesuaikan
-    'host': '192.168.157.50',
-    'user': 'pandika',
-    'pass': 'pandika',
-    'db_name': 'ims_1'
-}
-
-
-DB_4 = {
-    'host': 'localhost',
-    'user': 'root',
-    'pass': '',
-    'db_name': 'ims_1'
-}
-
-
-DB_5 = {
-    'host': '127.0.0.1',
-    'user': 'root',
-    'pass': '',
-    'db_name': 'ims_2'
+    'host': '192.168.157.52',
+    'user': 'firelord',
+    'pass': '123456',
+    'db_name': 'praktikum_ims_1'
 }
--- engine.py
+++ engine.py
@@ -5,7 +5,7 @@ tables = (
     {"name": "invoices", "id": "id"},
 )
 
-integration = Integration(config.DB_4, config.DB_5, tables)
+integration = Integration(config.DB_1, config.DB_2, tables)
 integration.run()
 while 1:
    pass
