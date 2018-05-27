6bbb1c3df064e7a66eb671f5d464379eacf30dde
trioputrap <trioputrap@gmail.com>
2018-05-28 05:39:16 +0800

    new ver

--- engine.py
+++ engine.py
@@ -5,7 +5,7 @@ tables = (
     {"name": "invoices", "id": "id"},
 )
 
-integration = Integration(config.DB_4, config.DB_5, tables)
+integration = Integration(config.DB_2, config.DB_1, tables)
 integration.run()
 while 1:
    pass
